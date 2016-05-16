a236d94c59aeac0249c31fae96690a9f5fbb7c8a
jake cazden <jacazden@gmail.com>
2016-05-15 21:54:09 -0600

    0.0.1

---
+++ Source/FourSouls/FourSoulsGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "FourSoulsGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FOURSOULS_API AFourSoulsGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
